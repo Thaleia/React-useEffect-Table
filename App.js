2f4539cb8c533655c3134b120592a04bfd23160f
Vavavoom <42918656+Thaleia@users.noreply.github.com>
2023-09-07 23:32:22 +0300

    updated App.js

---
+++ App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import FetchApi from './FetchApi';
+import ValidateInputField from './ValidateInputField';
+
+function App() {
+  return (
+    <div className="App">
+      <FetchApi />
+      <br />
+      <ValidateInputField />
+    </div>
+  );
+}
+
+export default App;
